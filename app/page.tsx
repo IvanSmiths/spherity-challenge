c1edb8fd441824815ee777085ade61dafc4b21dc
IvanSmiths <info@ivansmiths.com>
2024-12-10 17:21:17 +0100

    add basic fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div className=""></div>;
+import { getBattery } from "@/services/getBattery";
+
+export default async function Home() {
+  let battery = null;
+  try {
+    battery = await getBattery();
+  } catch (error) {
+    console.error("Error fetching battery data:", error);
+  }
+
+  if (!battery) {
+    return <div>Something went wrong on our side. Please notify us.</div>;
+  }
+  return <div className="">Battery data: {JSON.stringify(battery)}</div>;
 }
---
+++ services/getBattery.ts
@@ -0,0 +1,15 @@
+const API_BATTERY_ENDPOINT: string | undefined =
+  process.env.API_BATTERY_ENDPOINT;
+
+if (!API_BATTERY_ENDPOINT) {
+  throw new Error("API endpoint is not defined.");
+}
+
+export async function getBattery() {
+  const res: Response = await fetch(`${API_BATTERY_ENDPOINT}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
