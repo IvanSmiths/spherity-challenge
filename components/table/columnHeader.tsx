eab5a0e2e133820661e59a4c90e105823606e3d1
IvanSmiths <info@ivansmiths.com>
2024-12-10 18:23:26 +0100

    set column header as reusable component

---
+++ components/table/columnHeader.tsx
@@ -0,0 +1,20 @@
+import { Material } from "@/types/materialTypes";
+import { HeaderContext } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
+
+export const columnHeader = (title: string) => {
+  const HeaderComponent = ({ column }: HeaderContext<Material, unknown>) => (
+    <Button
+      variant="ghost"
+      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    >
+      {title}
+      <ArrowUpDown className="ml-2 h-4 w-4" />
+    </Button>
+  );
+
+  HeaderComponent.displayName = `HeaderComponent(${title})`;
+
+  return HeaderComponent;
+};
--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,57 +1,20 @@
 "use client";
 
-import { ColumnDef, HeaderContext } from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 import { Material } from "@/types/materialTypes";
-import { Button } from "@/components/ui/button";
-import { ArrowUpDown } from "lucide-react";
+import { columnHeader } from "@/components/table/columnHeader";
 
 export const columns: ColumnDef<Material>[] = [
   {
     accessorKey: "materialName",
-    header: ({ column }: HeaderContext<Material, unknown>) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={(): void =>
-            column.toggleSorting(column.getIsSorted() === "asc")
-          }
-        >
-          Name
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: columnHeader("Name"),
   },
   {
     accessorKey: "materialWeight",
-    header: ({ column }: HeaderContext<Material, unknown>) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={(): void =>
-            column.toggleSorting(column.getIsSorted() === "asc")
-          }
-        >
-          Weight
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: columnHeader("Weight"),
   },
   {
     accessorKey: "materialPercentageMassFraction",
-    header: ({ column }: HeaderContext<Material, unknown>) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={(): void =>
-            column.toggleSorting(column.getIsSorted() === "asc")
-          }
-        >
-          Percentage Mass Fraction
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
+    header: columnHeader("Percentage Mass Fraction"),
   },
 ];
