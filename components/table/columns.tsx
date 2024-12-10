870e2682b0d4b848866e1a363ff1595f7f83c227
IvanSmiths <info@ivansmiths.com>
2024-12-10 18:09:38 +0100

    add sorting functionality

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,19 +1,57 @@
 "use client";
 
-import { ColumnDef } from "@tanstack/react-table";
+import { ColumnDef, HeaderContext } from "@tanstack/react-table";
 import { Material } from "@/types/materialTypes";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
 
 export const columns: ColumnDef<Material>[] = [
   {
     accessorKey: "materialName",
-    header: "Material Name",
+    header: ({ column }: HeaderContext<Material, unknown>) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={(): void =>
+            column.toggleSorting(column.getIsSorted() === "asc")
+          }
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "materialWeight",
-    header: "Material Weight",
+    header: ({ column }: HeaderContext<Material, unknown>) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={(): void =>
+            column.toggleSorting(column.getIsSorted() === "asc")
+          }
+        >
+          Weight
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "materialPercentageMassFraction",
-    header: "Material Perc. Mass Franction",
+    header: ({ column }: HeaderContext<Material, unknown>) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={(): void =>
+            column.toggleSorting(column.getIsSorted() === "asc")
+          }
+        >
+          Percentage Mass Fraction
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
 ];
