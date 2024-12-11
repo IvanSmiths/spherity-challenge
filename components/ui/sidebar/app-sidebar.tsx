38ff3b7e53e7237845d966f9fcf765a80233b591
IvanSmiths <info@ivansmiths.com>
2024-12-11 11:02:54 +0100

    set links to a separate component

--- components/ui/sidebar/app-sidebar.tsx
+++ components/ui/sidebar/app-sidebar.tsx
@@ -12,81 +12,7 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar/sidebar";
 import * as React from "react";
-
-const data = {
-  navMain: [
-    {
-      title: "Battery",
-      url: "#",
-      items: [
-        {
-          title: "Battery Section",
-          url: "#",
-        },
-        {
-          title: "Info",
-          url: "#BatteryInfo",
-        },
-      ],
-    },
-    {
-      title: "Materials",
-      url: "#Materials",
-      items: [
-        {
-          title: "Materials Section",
-          url: "#Materials",
-        },
-        {
-          title: "Active Materials",
-          url: "#Active Materials",
-        },
-        {
-          title: "Electrolyte Composition",
-          url: "#Electrolyte Composition",
-        },
-      ],
-    },
-    {
-      title: "Charts",
-      url: "#",
-      items: [
-        {
-          title: "Charts Section",
-          url: "#Charts",
-        },
-        {
-          title: "Active Materials Charts",
-          url: "#Active MaterialsChart",
-        },
-        {
-          title: "Electrolyte Composition",
-          url: "#Electrolyte CompositionChart",
-        },
-      ],
-    },
-    {
-      title: "Performance",
-      url: "#Performance Metrics",
-      items: [
-        {
-          title: "Performance Section",
-          url: "#Performance Metrics",
-        },
-      ],
-    },
-    {
-      title: "More data",
-      url: "#More Data",
-      items: [
-        {
-          title: "More Data Section",
-          url: "#More Data",
-        },
-      ],
-    },
-  ],
-};
+import { links } from "@/lib/links";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -96,7 +22,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <Separator />
       <SidebarContent>
-        {data.navMain.map((item) => (
+        {links.navMain.map((item) => (
           <SidebarGroup key={item.title}>
             <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
             <SidebarGroupContent>
---
+++ lib/links.ts
@@ -0,0 +1,74 @@
+export const links = {
+  navMain: [
+    {
+      title: "Battery",
+      url: "#",
+      items: [
+        {
+          title: "Battery Section",
+          url: "#",
+        },
+        {
+          title: "Info",
+          url: "#BatteryInfo",
+        },
+      ],
+    },
+    {
+      title: "Materials",
+      url: "#Materials",
+      items: [
+        {
+          title: "Materials Section",
+          url: "#Materials",
+        },
+        {
+          title: "Active Materials",
+          url: "#Active Materials",
+        },
+        {
+          title: "Electrolyte Composition",
+          url: "#Electrolyte Composition",
+        },
+      ],
+    },
+    {
+      title: "Charts",
+      url: "#",
+      items: [
+        {
+          title: "Charts Section",
+          url: "#Charts",
+        },
+        {
+          title: "Active Materials Charts",
+          url: "#Active MaterialsChart",
+        },
+        {
+          title: "Electrolyte Composition",
+          url: "#Electrolyte CompositionChart",
+        },
+      ],
+    },
+    {
+      title: "Performance",
+      url: "#Performance Metrics",
+      items: [
+        {
+          title: "Performance Section",
+          url: "#Performance Metrics",
+        },
+      ],
+    },
+    {
+      title: "More data",
+      url: "#More Data",
+      items: [
+        {
+          title: "More Data Section",
+          url: "#More Data",
+        },
+      ],
+    },
+  ],
+};
